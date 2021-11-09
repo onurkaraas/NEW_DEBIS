970d9b62b6cfee8e8b705a4bdcde800f3f9a8129
Onur Karaaş <onurkaraas52@gmail.com>
2021-11-09 19:42:53 +0300

    ignore

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,13 +1,13 @@
 import React, {useState, useContext} from 'react';
 import {Button, Switch, Input} from 'react-native-elements';
-import {Text, View, Animated} from 'react-native';
+import {Text, View, Animated, StyleSheet} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import FastImage from 'react-native-fast-image';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useKeyboard, useDimensions} from '@react-native-community/hooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {COLORS, FONTS, SHADOWS} from '../constants/theme';
+import {COLORS, FONTS, LAYOUT, SHADOWS} from '../constants/theme';
 import {AuthContext} from '../context/AuthContext';
 import {TopBar} from '../components';
 
@@ -22,15 +22,10 @@ const LogInScreen = () => {
   const keyboard = useKeyboard();
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
+    <SafeAreaView style={styles.container}>
       {TopBar('Lütfen Giriş Yapınız')}
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Animated.View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
+      <View style={{...LAYOUT.setFlex1, ...LAYOUT.alignCenter}}>
+        <Animated.View style={{...LAYOUT.setFlex1, ...LAYOUT.justifyCenter}}>
           <FastImage
             style={{
               width: keyboard.keyboardShown ? width * 0.55 : width * 0.7,
@@ -44,18 +39,20 @@ const LogInScreen = () => {
           />
         </Animated.View>
 
-        <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={styles.inputContainer}>
           <Input
             onChangeText={setUsername}
             defaultValue={username}
             value={username}
             inputStyle={{color: COLORS.white}}
+            inputContainerStyle={styles.inputContainerStyle}
             containerStyle={{
               marginBottom: 8,
+              borderRadius: 12,
               ...SHADOWS.input,
+              backgroundColor: COLORS.primary,
               height: keyboard.keyboardShown ? height * 0.1 : height * 0.095,
               width: keyboard.keyboardShown ? width * 0.95 : width * 0.925,
-              backgroundColor: COLORS.primary,
             }}
             style={{
               height: keyboard.keyboardShown ? height * 0.1 : height * 0.095,
@@ -64,20 +61,9 @@ const LogInScreen = () => {
               borderRadius: 12,
               padding: 8,
               ...FONTS.body3,
-              justifyContent: 'center',
-              alignItems: 'center',
             }}
             leftIcon={
-              <View
-                style={{
-                  backgroundColor: '#625B39',
-                  height: 48,
-                  width: 38,
-                  flexDirection: 'column',
-                  borderRadius: 12,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
+              <View style={styles.inputUsernameIcon}>
                 <MaterialCommunityIcons
                   name={'account'}
                   color={COLORS.yellow}
@@ -86,21 +72,12 @@ const LogInScreen = () => {
               </View>
             }
             placeholder="Kullanıcı Adınız"
-            inputContainerStyle={{
-              borderBottomWidth: 0,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
           />
           <Input
             value={password}
             onChangeText={setPassword}
             defaultValue={password}
-            inputContainerStyle={{
-              borderBottomWidth: 0,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
+            inputContainerStyle={styles.inputContainerStyle}
             containerStyle={{
               marginBottom: keyboard.keyboardShown ? 8 : 16,
               ...SHADOWS.input,
@@ -116,19 +93,9 @@ const LogInScreen = () => {
               borderRadius: 12,
               padding: 8,
               ...FONTS.body3,
-              justifyContent: 'center',
-              alignItems: 'center',
             }}
             leftIcon={
-              <View
-                style={{
-                  backgroundColor: '#623A42',
-                  height: 48,
-                  width: 38,
-                  borderRadius: 12,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
+              <View style={styles.inputPasswordIcon}>
                 <MaterialCommunityIcons
                   name={'lock'}
                   color={COLORS.red}
@@ -155,15 +122,14 @@ const LogInScreen = () => {
               marginTop: 10,
               alignItems: 'center',
             }}>
-            <View style={{justifyContent: 'center'}}>
+            <View>
               <Button
                 title={'Giriş Yap'}
                 buttonStyle={{
-                  justifyContent: 'center',
                   backgroundColor: COLORS.green,
+                  borderRadius: 10,
                   height: height * 0.07,
                   width: width * 0.7,
-                  borderRadius: 10,
                 }}
                 titleStyle={{
                   ...FONTS.h2,
@@ -184,15 +150,7 @@ const LogInScreen = () => {
           </View>
           <View style={{alignItems: 'flex-end', padding: 12}}>
             <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-              <Text
-                style={{
-                  color: COLORS.white,
-                  ...FONTS.body3,
-                  fontWeight: 'bold',
-                  marginRight: 12,
-                }}>
-                Beni Hatırla
-              </Text>
+              <Text style={styles.saveText}>Beni Hatırla</Text>
               <Switch
                 style={{transform: [{scaleX: 1.4}, {scaleY: 1.4}]}}
                 value={saveUser}
@@ -206,5 +164,37 @@ const LogInScreen = () => {
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: COLORS.primary},
+  inputContainer: {flex: 1, flexDirection: 'column'},
+  inputContainerStyle: {
+    borderBottomWidth: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputUsernameIcon: {
+    backgroundColor: '#625B39',
+    height: 48,
+    width: 38,
+    flexDirection: 'column',
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputPasswordIcon: {
+    backgroundColor: '#623A42',
+    height: 48,
+    width: 38,
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  saveText: {
+    color: COLORS.white,
+    ...FONTS.body3,
+    fontWeight: 'bold',
+    marginRight: 12,
+  },
+});
 
 export default LogInScreen;
